05878bd7fdee8b9b2dd9e79f4422f28200bef904
twilightDF <1159990340@qq.com>
2018-05-27 17:33:03 +0800

    add fuection say goog byb

--- HelloWorld/src/com/demo/HelloWorld.java
+++ HelloWorld/src/com/demo/HelloWorld.java
@@ -3,9 +3,15 @@ package com.demo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 10; i++) {
-			System.out.println("Hello World dufeng");
+		for(int i = 0; i < 20; i++) {
+			sayHello("defeng");
 		}
-			}
+		
+		sayHello("good bye");
+	}
+	
+	private static void sayHello(String name) {
+		System.out.println("Hello World " + name);
+	}
 
 }
